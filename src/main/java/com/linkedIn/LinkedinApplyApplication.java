ae3af1124a07f4aa1e16b9b07813108a67fc2f31
Goutam Sikder <goutam.sikder@gmail.com>
2017-09-20 17:25:15 +0530

    First Commit of Full Projects

---
+++ src/main/java/com/linkedIn/LinkedinApplyApplication.java
@@ -0,0 +1,12 @@
+package com.linkedIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinApplyApplication.class, args);
+	}
+}
---
+++ src/main/java/com/linkedIn/controller/LinkedInController.java
@@ -0,0 +1,41 @@
+package com.linkedIn.controller;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.linkedin.api.LinkedIn;
+import org.springframework.social.linkedin.api.ProfileOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/linkedin")
+public class LinkedInController {
+
+	private LinkedIn linkedIn;
+    private ConnectionRepository connectionRepository;
+ 
+    public LinkedInController(LinkedIn linkedIn, ConnectionRepository connectionRepository) {
+        this.linkedIn = linkedIn;
+   
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloLinkedIn(Model model) {
+        if (connectionRepository.findPrimaryConnection(LinkedIn.class) == null) {
+            return "redirect:/connect/linkedin";
+            
+        }
+        
+        //ProfileOperations user = linkedIn.profileOperations();
+
+        model.addAttribute("linkedInProfile",linkedIn.profileOperations().getUserProfileFull());
+        
+       
+        return "linkedin";
+    }
+    
+    
+
+}
